e286c1406859afbceaebf54903c3ca9f8d118efd
Sebastianthomas94 <Sebastianthomas94@outlook.com>
2019-08-06 16:13:48 +0530

    6-08-2019

--- router1/routes/product.js
+++ router1/routes/product.js
@@ -8,6 +8,50 @@ module.exports=router;
 router.set("views", "../views");   */   
 
 router.get("/", function(req,res){
-    console.log("reached!");
+    console.log("reached router!");
     res.render("../views/product");
 });
+
+
+router.get("/new", function(req,res){
+    console.log("reached new product!");
+    res.render("../views/newproduct");
+});
+router.get("/edit", function(req,res){
+    console.log("reached edit product!");
+    res.render("../views/editproduct");
+});
+router.get("/view", function(req,res){
+    console.log("reached view!");
+    res.render("../views/viewproduct");
+});
+router.post("/del", function(req,res){
+    console.log("reached delete!");
+    res.render("../views/delproduct",{pname:proname, price:proprice, qnty:proqtn, i:i });
+});
+
+/* router.get("/added", function(req,res){
+    console.log("reached added!");
+    res.render("../views/added");
+});
+ */
+
+
+
+
+
+var i=0;
+var proname=Array(), proprice=[""], proqtn=[""];
+router.post("/added", function(req,res){
+    proname[i]=req.body.pname;console.log(proname[i]);
+    proprice[i]=req.body.price;console.log(proprice[i]);
+    proqtn[i]=req.body.qnty; console.log(proqtn[i]);
+    i++; 
+    res.render("added",{pname:req.body.pname, price:req.body.price, qnty:req.body.qnty, i:i });
+});
+
+router.get("/del", function(req,res){
+    console.log("reached delete!");
+    res.render("../views/delproduct");
+});
+
